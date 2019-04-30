55b3e9f82e6098d8ea726bc1b924e261675cadff
User2 <User2@desktop246>
2019-04-30 12:30:39 +0100

    Project update pom and acc config

--- src/main/java/pl/piomin/microservices/account/AccountConfiguration.java
+++ src/main/java/pl/piomin/microservices/account/AccountConfiguration.java
@@ -1,17 +1,18 @@
 package pl.piomin.microservices.account;
 
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import brave.sampler.Sampler;
 
 @Configuration
 @ComponentScan("pl.piomin.microservices.account")
 public class AccountConfiguration {
 
 	@Bean
-	public AlwaysSampler defaultSampler() {
-	  return new AlwaysSampler();
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 	
 }
